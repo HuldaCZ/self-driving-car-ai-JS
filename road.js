e77bb535743761e8cf628111cb75296d668d2f96
HuldaCZ <phulinsky@gmail.com>
2022-09-11 23:11:04 +0200

    Self Driving Car

---
+++ road.js
@@ -0,0 +1,51 @@
+class Road {
+  constructor(x, width, laneCount = 3) {
+    this.x = x;
+    this.width = width;
+    this.laneCount = laneCount;
+
+    this.left = x - width / 2;
+    this.right = x + width / 2;
+
+    const infinity = 1000000;
+    this.top = -infinity;
+    this.bottom = infinity;
+
+    const topLeft = { x: this.left, y: this.top };
+    const topRight = { x: this.right, y: this.top };
+    const bottomLeft = { x: this.left, y: this.bottom };
+    const bottomRight = { x: this.right, y: this.bottom };
+
+    this.borders = [
+      [topLeft, bottomLeft],
+      [topRight, bottomRight],
+    ];
+  }
+
+  getLaneCenter(laneIndex) {
+    const lineWidth = this.width / this.laneCount;
+    return this.left + lineWidth / 2 + Math.min(laneIndex, this.laneCount - 1) * lineWidth;
+  }
+
+  draw(ctx) {
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "white";
+
+    for (let i = 1; i <= this.laneCount - 1; i++) {
+      const x = lerp(this.left, this.right, i / this.laneCount);
+      ctx.setLineDash([20, 20]);
+      ctx.beginPath();
+      ctx.moveTo(x, this.top);
+      ctx.lineTo(x, this.bottom);
+      ctx.stroke();
+    }
+
+    ctx.setLineDash([]);
+    this.borders.forEach(([p1, p2]) => {
+      ctx.beginPath();
+      ctx.moveTo(p1.x, p1.y);
+      ctx.lineTo(p2.x, p2.y);
+      ctx.stroke();
+    });
+  }
+}
